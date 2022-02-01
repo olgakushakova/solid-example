223a93edee4b3b3e8803afb642ce33bf3b1d0595
olgakushakova <ola.kushak@gmail.com>
2022-02-02 00:25:51 +0600

    Change to area for decoration

--- src/main/java/solid/example/controllers/CalculateArea.java
+++ src/main/java/solid/example/controllers/CalculateArea.java
@@ -22,18 +22,6 @@ public class CalculateArea {
         return area;
     }
 
-    public double calculateCircleArea() {
-
-        Double circleArea;
-
-        Scanner sn = new Scanner(System.in);
-        System.out.println("Enter radius of the circle: ");
-        Double radius = sn.nextDouble();
-
-        circleArea = Math.PI * radius * radius;
-
-        return circleArea;
-    }
 
     public double calculateSquareArea() {
 
@@ -46,6 +34,19 @@ public class CalculateArea {
         squareArea = length * length;
 
         return squareArea;
+    }
+
+    public double calculateCircleArea() {
+
+        Double circleArea;
+
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Enter radius of the circle: ");
+        Double radius = sn.nextDouble();
+
+        circleArea = Math.PI * radius * radius;
+
+        return circleArea;
     }
 
     public double calculateTriangleArea() {
@@ -74,6 +75,62 @@ public class CalculateArea {
         Double height = sn.nextDouble();
 
         rectangleArea = length * height;
+
+        return rectangleArea;
+    }
+
+    public double calculateSquareAreaForDecoration() {
+
+        Double squareArea;
+
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Enter length of the square side: ");
+        Double length = sn.nextDouble();
+
+        squareArea = length * length * 1.1;
+
+        return squareArea;
+    }
+
+    public double calculateCircleAreaForDecoration() {
+
+        Double circleArea;
+
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Enter radius of the circle: ");
+        Double radius = sn.nextDouble();
+
+        circleArea = Math.PI * radius * radius * 1.2;
+
+        return circleArea;
+    }
+
+    public double calculateTriangleAreaForDecoration() {
+
+        Double triangleArea;
+
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Enter the length of the triangle base: ");
+        Double length = sn.nextDouble();
+        System.out.println("Enter the length of the triangle height: ");
+        Double height = sn.nextDouble();
+
+        triangleArea = length * height / 2 * 1.2;
+
+        return triangleArea;
+    }
+
+    public double calculateRectangleAreaForDecoration() {
+
+        Double rectangleArea;
+
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Enter the length of the rectangle base: ");
+        Double length = sn.nextDouble();
+        System.out.println("Enter the height of the rectangle: ");
+        Double height = sn.nextDouble();
+
+        rectangleArea = length * height * 1.1;
 
         return rectangleArea;
     }
