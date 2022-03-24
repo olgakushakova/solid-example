5bc2ce7d517a302eaf87006d6918aa52f2345c4c
olgakushakova <ola.kushak@gmail.com>
2022-03-25 01:31:05 +0600

    Fix coeff

--- src/main/java/solid/example/calculations/CalculatePaintingArea.java
+++ src/main/java/solid/example/calculations/CalculatePaintingArea.java
@@ -5,7 +5,7 @@ import solid.example.models.*;
 
 public class CalculatePaintingArea implements CalculateArea {
 
-    private static final Double PAINTING_COEFFICIENT = 1.2;
+    private static final Double PAINTING_COEFFICIENT = 1.1;
 
     @Override
     public Double calculateArea(Figure figure) {
--- src/main/java/solid/example/calculations/CalculateTileArea.java
+++ src/main/java/solid/example/calculations/CalculateTileArea.java
@@ -5,7 +5,7 @@ import solid.example.models.*;
 
 public class CalculateTileArea implements CalculateArea {
 
-    private static final Double TILE_COEFFICIENT = 1.4;
+    private static final Double TILE_COEFFICIENT = 1.2;
 
     @Override
     public Double calculateArea(Figure figure) {
