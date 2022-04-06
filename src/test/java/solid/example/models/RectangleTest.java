0697d2fe6d5b8c0ae97b3405ca5bf70e580e7f58
olgakushakova <ola.kushak@gmail.com>
2022-04-06 10:49:55 +0600

    Add tests for rectangle and square

--- src/test/java/solid/example/models/RectangleTest.java
+++ src/test/java/solid/example/models/RectangleTest.java
@@ -2,8 +2,6 @@ package solid.example.models;
 
 import org.junit.jupiter.api.Test;
 
-import java.sql.SQLOutput;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RectangleTest {
